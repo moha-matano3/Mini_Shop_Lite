dccd155b72974614635087e9c0fc9a6eb4780bf3
moha-matano3 <matanomohamed3@gmail.com>
2025-10-02 10:49:55 +0300

    Installation of breeze, creation of models and controllers & seeding of users to dB

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,23 +1,41 @@
 <?php
 
-namespace Database\Seeders;
+    namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+    use App\Models\User;
+    use Illuminate\Database\Seeder;
+    use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    class DatabaseSeeder extends Seeder
     {
-        // User::factory(10)->create();
+        /**
+         * Seed the application's database.
+         */
+        public function run(): void
+        {
+            
+            User::factory()->create([
+                'name' => 'Admin User',
+                'email' => 'admin@demo.com',
+                'password' => Hash::make('password'),
+                'userType' => 'Admin',
+            ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            
+            User::factory()->create([
+                'name' => 'Customer User',
+                'email' => 'customer@demo.com',
+                'password' => Hash::make('password'),
+                'userType' => 'Customer',
+            ]);
+
+            User::factory()->create([
+                'name' => 'Superadmin User',
+                'email' => 'super@demo.com',
+                'password' => Hash::make('password'),
+                'userType' => 'SuperAdmin',
+            ]);
+        }
     }
-}
+
+?>
